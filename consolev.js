5fef8794f9be0a212e05afba8a55b5d52b18f540
Vincent <const.lomo@gmail.com>
2017-10-23 16:01:34 +0800

    init

---
+++ consolev.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Vincent on 2017/10/23.
+ */
+module.exports = function () {
+  console.log('%c [%s] %s ', `background: ${process.env.NODE_ENV === 'production'?'#000':'#f00'}; color: #fff;`, process.env.NODE_ENV, Array.prototype.slice.call(arguments).toString());// eslint-disable-line
+};
