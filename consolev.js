859e7526825fd78d49a1091d4a98fa549ac09dec
Vincent <const.lomo@gmail.com>
2018-08-23 09:15:10 +0800

    upgrade

--- consolev.js
+++ consolev.js
@@ -1,6 +1,13 @@
 /**
  * Created by Vincent on 2017/10/23.
  */
-module.exports = function () {
-  console.log('%c [%s] %s ', 'background: ' + (process.env.NODE_ENV === 'production'?'#000':'#f00') + '; color: #fff;', process.env.NODE_ENV, Array.prototype.slice.call(arguments).toString());// eslint-disable-line
+module.exports = function() {
+  console.log(
+    '%c [%s] %s ',
+    'background: ' +
+      (process.env.NODE_ENV === 'production' ? '#000' : '#f00') +
+      '; color: #fff;',
+    process.env.NODE_ENV,
+    v ? Array.prototype.slice.call(arguments).toString() : process.env.VERSION
+  ); // eslint-disable-line
 };
