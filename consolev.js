1b75d54d4a12426fdac421c207c9c9d85b0d0606
Vincent <const.lomo@gmail.com>
2018-08-23 14:47:23 +0800

    upgrade

--- consolev.js
+++ consolev.js
@@ -1,15 +1,26 @@
-/**
- * Created by Vincent on 2017/10/23.
- */
-module.exports = function(v) {
+/* eslint-disable */
+const version = process.env.VERSION || process.env.REACT_APP_VERSION;
+module.exports = function() {
+  const args = Array.prototype.slice
+    .call(arguments)
+    .map(val => {
+      if (val === undefined) {
+        return 'undefined';
+      } else if (typeof val === 'string') {
+        return val;
+      }
+      try {
+        return JSON.stringify(val);
+      } catch (error) {
+        return '[TypeError]';
+      }
+    })
+    .join(' ');
   console.log(
-    '%c [%s] %s ',
-    'background: ' +
-      (process.env.NODE_ENV === 'production' ? '#000' : '#f00') +
-      '; color: #fff;',
-    process.env.NODE_ENV,
-    v
-      ? Array.prototype.slice.call(arguments).toString()
-      : process.env.VERSION || process.env.REACT_APP_VERSION
-  ); // eslint-disable-line
+    `%c [${process.env.NODE_ENV}] ${version} %c ${args}`,
+    `background: ${
+      process.env.NODE_ENV === 'production' ? '#000' : '#f00'
+    }; color: #fff;`,
+    ``
+  );
 };
