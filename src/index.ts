ae95ab25bae671349cf760a676e39f91fb806091
lomocc <constlomo@gmail.com>
2021-06-15 12:57:26 +0800

    fix: fix type

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,27 @@
-export type ConsolevArg = [message: string, style?: string] | string;
+export type ConsolevArgMessage = string | boolean | null | undefined;
+
+export type ConsolevArg = [message: ConsolevArgMessage, style?: string];
+
+export type ConsolevArgLike = ConsolevArg | ConsolevArgMessage;
 
 const nodeEnv = process.env.NODE_ENV;
 
-export default function consolev(...args: ConsolevArg[]) {
-  args = args
+const prefixArgs: ConsolevArg[] = [
+  [
+    ` ${nodeEnv} `,
+    `background-color: ${
+      nodeEnv === 'production' ? '#000' : '#f00'
+    }; color: #fff;`,
+  ],
+];
+
+export default function consolev(...args: ConsolevArgLike[]) {
+  const consolevArgs = args
     .map((arg) => (Array.isArray(arg) ? arg : ([arg] as ConsolevArg)))
     .filter((arg) => arg[0] !== void 0);
 
-  const prefixArgs: ConsolevArg[] = [
-    [
-      ` ${nodeEnv} `,
-      `background-color: ${
-        nodeEnv === 'production' ? '#000' : '#f00'
-      }; color: #fff;`,
-    ],
-  ];
-  const optionalArgs = [...prefixArgs, ...args];
+  const optionalArgs = [...prefixArgs, ...consolevArgs];
+
   const message = '%c%s%c '.repeat(optionalArgs.length);
 
   const optionalParams = optionalArgs
